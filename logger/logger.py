7131feb113e9f10e97cd329076c3aae2f3f1508c
kunal.sharma <kunal.sharma@unbxd.com>
2018-01-08 19:55:04 +0530

    refact(Kite) - Nullhandler + file handler + email handler

--- logger/logger.py
+++ logger/logger.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from .log_buffer import KiteErrorsBufferEmailHandler
+from logging import NullHandler
 
 
 class KiteLogger:
@@ -8,28 +9,41 @@ class KiteLogger:
     Class to manage logs
     """
 
-    def __init__(self, log_directory='/tmp', log_file='kite-logger.log',
-                 log_level=logging.DEBUG):
+    def __init__(self, log_level=logging.DEBUG):
+        """
+        Registers the logger
 
+        :param log_level: the default log level for the logger
+        """
+        self.log_level = log_level
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+        # adding Null Handler - https://docs.python.org/3/howto/logging.html#library-config
+        self.logger.addHandler(NullHandler())
+
+        self.logger.setLevel(self.log_level)
+        self.format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    def add_file_handler(self, log_directory='/tmp', log_file='kite-logger.log'):
+        """
+        :param log_directory: folder location for the file
+        :param log_file: file name
+        :return: None
+        """
+        # adding file handler
         if not os.path.exists(log_directory):
             os.makedirs(log_directory)
         log_location = '{}/{}'.format(log_directory, log_file)
-        self.logger = logging.getLogger(self.__class__.__name__)
-        self.logger.setLevel(log_level)
-
-        self.format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-        # adding file handler
         fh = logging.FileHandler(log_location, mode='a')
         fh.setFormatter(self.format)
-        fh.setLevel(log_level)
+        fh.setLevel(self.log_level)
         self.logger.addHandler(fh)
 
-    def add_handler(self, **kwargs):
+    def add_email_handler(self, **kwargs):
         """
         Additional handler to handle emails
         """
-        kebeh = KiteErrorsBufferEmailHandler(capacity=2,
+        kebeh = KiteErrorsBufferEmailHandler(capacity=kwargs.get('capacity', 1),
                                              toaddrs=kwargs['email_to'],
                                              subject=kwargs['email_subject'])
         kebeh.setFormatter(self.format)
