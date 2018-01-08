c5f54f90d6503a6218277e4a8b206898e3b501ef
kunal.sharma <kunal.sharma@unbxd.com>
2018-01-08 19:56:03 +0530

    refact(Kite-tests) - added more unit tests

--- tests/test_kite_logger.py
+++ tests/test_kite_logger.py
@@ -6,7 +6,28 @@ from logger.logger import KiteLogger
 
 class TestFileLogger(TestCase):
 
-    def test_file_logging(self):
+    def test_null_handler(self):
+        log = KiteLogger()
+        try:
+            log.logger.debug("DEBUG: Hello World")
+            log.logger.info("DEBUG: Hello Sansaar")
+            log.logger.error("ERROR: Hello Duniya")
+            log.logger.critical("CRITICAL: Hello People")
+        except:
+            assert False
+
+    def test_file_handler(self):
+        log = KiteLogger()
+        log.add_file_handler()
+        try:
+            log.logger.debug("DEBUG: Hello World")
+            log.logger.info("DEBUG: Hello Sansaar")
+            log.logger.error("ERROR: Hello Duniya")
+            log.logger.critical("CRITICAL: Hello People")
+        except:
+            assert False
+
+    def test_stream_handler(self):
         log = KiteLogger()
 
         stream_handler = logging.StreamHandler(sys.stdout)
